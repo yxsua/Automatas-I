90a1a8a8b2bab5f15b2df8911298145d5ada900c
yxsua <yx.suaa@gmail.com>
2024-02-02 15:47:25 -0600

    test2

--- HolaMundo/obj/Debug/net8.0/HolaMundo.AssemblyInfo.cs
+++ HolaMundo/obj/Debug/net8.0/HolaMundo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37ea40adadf4678079f420daf84334d921ad0f49")]
 [assembly: System.Reflection.AssemblyProductAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HolaMundo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
